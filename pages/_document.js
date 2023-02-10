a2f1b09cc99a3d2e67a98d2f1e2fc91df378b999
Fayaz Basheer <pnfayaz@gmail.com>
2023-02-10 20:41:52 +0400

    add signup with github and save to firestore

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <style>
+          @import
+          url(`https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&display=swap`);
+        </style>
+      </Head>
       <body>
         <Main />
         <NextScript />
